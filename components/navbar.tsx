a339abb8f0b8fb9d22320ea0d85a2652a3a3a482
David Sawires <davidashraf@gmail.com>
2024-04-20 02:50:29 -0500

    Resume Fix and Github Menubar links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -76,9 +76,6 @@ export const Navbar = () => {
 			</NavbarContent>
 
 	<NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal href={siteConfig.links.github}>
-          <GithubIcon className="text-default-500" />
-        </Link>
 		<NavbarMenuToggle className="xl:hidden" />
     </NavbarContent>
 
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -5,8 +5,7 @@ import DefaultLayout from "@/layouts/default";
 export default function resumePage() {
 	return (
 		<DefaultLayout>
-			<object data="/David_Sawires_Resume.pdf" type="application/pdf" style={{height : "100%", width : "100%"}}>
-			</object>
+<iframe src="https://drive.google.com/file/d/1Y6U2Twogox3b6JmjtVmU_zDG1dBQrF9O/preview" width="100%" height="100%" allow="autoplay"></iframe>
 		</DefaultLayout>
 	);
 }
